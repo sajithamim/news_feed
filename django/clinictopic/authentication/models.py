1c0436774f7c3a161a100b784ab8c4f92472c50c
ajith <ajith@metrictreelabs.com>
2021-06-18 14:26:38 +0530

    changes

--- django/clinictopic/authentication/models.py
+++ django/clinictopic/authentication/models.py
@@ -10,13 +10,15 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, username, email, password=None):
+    def create_user(self, username, email,phone,otp,password=None):
         if username is None:
             raise TypeError('Users should have a username')
         if email is None:
             raise TypeError('Users should have a Email')
+        if phone is None:
+            raise TypeError('Users should have a Phone number')
 
-        user = self.model(username=username, email=self.normalize_email(email))
+        user = self.model(username=username, email=self.normalize_email(email),phone=phone,otp=otp)
         user.set_password(password)
         user.save()
         return user
