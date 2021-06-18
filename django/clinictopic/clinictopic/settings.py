38409bb02ccbe8985422b85b51eae5a89faf1fee
ajith <ajith@metrictreelabs.com>
2021-06-18 14:20:15 +0530

    changes

--- django/clinictopic/clinictopic/settings.py
+++ django/clinictopic/clinictopic/settings.py
@@ -14,10 +14,10 @@ import os
 import datetime
 from dotenv import load_dotenv
 
-load_dotenv(BASE_DIR+str("/.env"))
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+load_dotenv(BASE_DIR+str("/.env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
