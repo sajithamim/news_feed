01a9231bf20b90472bb3970e41658eb7c4e5c485
Sony Tom <mailsonytom@gmail.com>
2021-06-24 13:17:12 +0530

    Updates login

--- react/clinic-admin/src/App.js
+++ react/clinic-admin/src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Overview from './pages/Overview';
+import Login from './pages/Login/Login';
 // import { Reports, ReportsOne, ReportsTwo, ReportsThree } from './pages/Reports';
 // import Team from './pages/Team';
 // import Specialization from './pages/specialization/Specialization';
 // import Sidebar from './components/Sidebar/Sidebar';
 import Topics from './pages/Topics/Topics';
-import AdminLayout from "./components/Layout/AdminLayout";
+import AdminLayout from "./Layouts/AdminLayout/AdminLayout";
 
 function App() {
   return (
     <Router>
       <AdminLayout>
         <Switch>
+          <Route path="/login" exact component={Login} />
           <Route path="/data" exact component={Overview} />
           {/* <Route path='/reports' exact component={Reports} /> */}
           <Route path="/data/topics" exact component={Topics} />
@@ -20,9 +22,10 @@ function App() {
           {/* <Route path='/reports/reports2' exact component={ReportsTwo} /> */}
           {/* <Route path='/reports/reports3' exact component={ReportsThree} /> */}
           {/* <Route path='/team' exact component={Team} /> */}
-          <Redirect from="/" to="/data" />
+          <Redirect from="/" to="/login" />
         </Switch>
       </AdminLayout>
+
     </Router>
   );
 }
