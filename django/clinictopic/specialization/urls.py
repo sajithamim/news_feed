e623638bebcd6327085c239288c2b6b5a1247a26
ajith <ajith@metrictreelabs.com>
2021-06-25 09:54:25 +0530

    changes

--- django/clinictopic/specialization/urls.py
+++ django/clinictopic/specialization/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import (GetSpecializations,GetAudienceView)
+from .views import (GetSpecializations,GetAudienceView,UserTypeView)
 
 urlpatterns = [
         path('getspec/', GetSpecializations.as_view()),
         path('getaudience/',GetAudienceView.as_view()),
+        path('usertype/',UserTypeView.as_view()),
 ]
