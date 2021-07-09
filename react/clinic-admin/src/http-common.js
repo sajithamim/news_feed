b8e101fd3be96d8c75f2e6b6fc41741d3861c3b2
Sajitha <sajithamim16@gmail.com>
2021-07-09 00:06:04 -0700

    added Token for API

--- react/clinic-admin/src/http-common.js
+++ react/clinic-admin/src/http-common.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
+const accessToken = localStorage.getItem('accessToken');
+
 export const http = axios.create({
   baseURL: "http://178.18.246.233:8000/",
   headers: {
-    "Content-type": "application/json"
+    "Content-type": "application/json",
+    "Authorization":  `Bearer ${accessToken}`
   }
 });
 
