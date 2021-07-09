b28ac0dd5d2d8b6cc9d73363974d5946571a23e4
ajith <ajith@metrictreelabs.com>
2021-07-09 19:47:06 +0530

    changes

--- django/clinictopic/authentication/serializers.py
+++ django/clinictopic/authentication/serializers.py
@@ -275,6 +275,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
 
 class ProfileUpdateSerializer(serializers.ModelSerializer):
+    profilepic = serializers.ImageField(max_length=None, use_url=True, allow_null=True, required=False)
     class Meta:
         model = User
         fields = ['profilepic']
