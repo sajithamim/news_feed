8c6d97e0ef48c4d682d54dbbda736e13259cfe49
ajith <ajith@metrictreelabs.com>
2021-08-12 10:50:10 +0530

    changes

--- django/clinictopic/poll/models.py
+++ django/clinictopic/poll/models.py
@@ -45,10 +45,10 @@ class Feedback(models.Model):
 
 
 class Settings(models.Model):
-    tos = models.CharField(max_length=3000,blank=True,null=True)
-    privacy_policy = models.CharField(max_length=3000,blank=True,null=True)
-    about_us = models.CharField(max_length=3000,blank=True,null=True)
-    contact_us = models.CharField(max_length=255,blank=True,null=True)
+    tos = models.CharField(max_length=100000,blank=True,null=True)
+    privacy_policy = models.CharField(max_length=100000,blank=True,null=True)
+    about_us = models.CharField(max_length=10000,blank=True,null=True)
+    contact_us = models.CharField(max_length=3000,blank=True,null=True)
 
     class Meta:
         db_table = 'Settings'
