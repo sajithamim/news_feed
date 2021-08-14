a140ae93ac82f42c66b261a8462f56d77b47fedd
ajith <ajith@metrictreelabs.com>
2021-08-14 12:12:26 +0530

    changes

--- django/clinictopic/specialization/admin.py
+++ django/clinictopic/specialization/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import (SubSpecialization,UserSubSpecialization,Audience,
-Specialization,UserSpecialization, UserType)
+Specialization,UserSpecialization, UserType,Advisory)
 
 allmodels = [SubSpecialization,UserSubSpecialization,Audience,
-Specialization,UserSpecialization, UserType]
+Specialization,UserSpecialization, UserType,Advisory]
 
 for allmodels in allmodels:
     admin.site.register(allmodels)
