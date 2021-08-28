7d8b1b2a17a6dd253cf64e0f5620ea4ae86ab7af
ajith <ajith@metrictreelabs.com>
2021-08-28 09:13:35 +0530

    changes

--- django/clinictopic/topics/serializers.py
+++ django/clinictopic/topics/serializers.py
@@ -94,6 +94,7 @@ class GetTopicSeriaizer(serializers.ModelSerializer):
     class Meta:
         model = Topics
         fields = '__all__'
+        
     def to_representation(self, instance):
         response = super().to_representation(instance)
         response['category_id'] = CategorySerializer(instance.category_id).data
