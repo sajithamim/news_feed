7a60be7d40e0f9d2e37f9370a7b94c5268c5a60d
SAJITHA K SOMAN <sajitha@metrictreelabs.com>
2021-10-01 06:06:16 -0700

    PRivacy ploicy , about us, terms and condition

--- react/clinic-admin/src/actions/settings.js
+++ react/clinic-admin/src/actions/settings.js
@@ -1,8 +1,8 @@
 import  Settings from "../services/Settings";
 
-export const getSettings = () => async(dispatch) => {
+export const getPrivacy = () => async(dispatch) => {
     try{
-        const res = await Settings.getSettings();
+        const res = await Settings.getPrivacy();
         return res;
     }
     catch (err) {
@@ -10,32 +10,90 @@ export const getSettings = () => async(dispatch) => {
     }
 }
 
-// export const patchSettings = (id ,state) => async(dispatch) => {
-//     try{
-//         const res = await Settings.patchSettings(id ,state);
-//         dispatch({
-//             type: 'POST_SETTINGS',
-//             payload: res.data,
-//         })
-//     }
-//     catch (err) {
-
-//     }
-// }
-
-export const postSettings = (state) => async(dispatch) => { 
-    console.log("state" , state);
+export const getAbout = () => async(dispatch) => {
     try{
-        const res = await Settings.postSettings(state);
-        dispatch({
-            type: 'POST_SETTINGS',
-            payload: res.data,
-        })
+        const res = await Settings.getAbout();
+        console.log("response get", res);
+        return res;
     }
     catch (err) {
 
     }
 }
 
+export const getTerms = () => async(dispatch) => {
+    try{
+        const res = await Settings.getTerms();
+        console.log("response get", res);
+        return res;
+    }
+    catch (err) {
 
+    }
+}
+
+export const getContact = () => async(dispatch) => {
+    try{
+        const res = await Settings.getContact();
+        console.log("response get", res);
+        return res;
+    }
+    catch (err) {
+
+    }
+}
+export const postPrivacy = (state) => async(dispatch) => { 
+    try{
+        const res = await Settings.postPrivacy(state);
+        console.log("res" , res);
+        dispatch({
+            type: 'POST_PRIVACY',
+            payload: res.data,
+        })
+     }
+    catch (err) {
+
+    }
+}
+export const postAbout = (state) => async(dispatch) => { 
+    try{
+        const res = await Settings.postAbout(state);
+        console.log("res" , res);
+        dispatch({
+            type: 'POST_ABOUT',
+            payload: res.data,
+        })
+     }
+    catch (err) {
+
+    }
+}
+
+export const postContact = (state) => async(dispatch) => { 
+    try{
+        const res = await Settings.postContact(state);
+        console.log("res" , res);
+        dispatch({
+            type: 'POST_CONTACT',
+            payload: res.data,
+        })
+     }
+    catch (err) {
+
+    }
+}
+
+export const postTerms = (state) => async(dispatch) => { 
+    try{
+        const res = await Settings.postTerms(state);
+        console.log("res" , res);
+        dispatch({
+            type: 'POST_TERMS',
+            payload: res.data,
+        })
+     }
+    catch (err) {
+
+    }
+}
 
--- react/clinic-admin/src/reducers/settings.js
+++ react/clinic-admin/src/reducers/settings.js
@@ -8,7 +8,7 @@ export default (state = initialState, action) => {
     switch (action.type) {
         case 'GET_SETTINGS':
         return {...state , settingsList: action.payload}
-        case 'POST_SETTINGS':
+        case 'POST_PRIVACY':
         return {...state , postSettingsList: action.payload}
         default:
         return state
