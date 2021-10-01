630453aa73fd90d0c71a9f7c95c6bbb61002934c
ob_sree <sreerag@metrictreelabs.com>
2021-10-01 11:23:53 +0530

    chnages

--- django/clinictopic/poll/urls.py
+++ django/clinictopic/poll/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf.urls import url, include
 from rest_framework import routers
-from .views import PollView,userPollview,Feedbackview,SettingsViewSet,ContactusView,AddSettingView
+from .views import ContactusApiView, PollView, PrivacyPolicyApiView, TOSapiView,userPollview,Feedbackview,SettingsViewSet,ContactusView,AddSettingView,AboutusApiview
 router = routers.DefaultRouter()
 router.register('contactus',ContactusView , 'contactus')
 router.register('addsetting',AddSettingView , 'addsetting')
@@ -12,4 +12,8 @@ urlpatterns = [
         path('userpoll/',userPollview.as_view()),
         path('feedback/',Feedbackview.as_view()),
         path('settings/',SettingsViewSet.as_view()),
+        path('aboutus/',AboutusApiview.as_view()),
+        path('contact/',ContactusApiView.as_view()),
+        path('privacypolicy/',PrivacyPolicyApiView.as_view()),
+        path('tos/',TOSapiView.as_view()),
 ]
--- django/clinictopic/poll/views.py
+++ django/clinictopic/poll/views.py
@@ -1,3 +1,4 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
@@ -8,12 +9,20 @@ from rest_framework import viewsets, filters
 # from rest_framework.decorators import detail_route
 from rest_framework.decorators import action
 from rest_framework import parsers
-from .serializers import TopicPollSerializer,UserPollSerializer,FeedbackSerializer,SettingsSerializer,ContactusSerializer,AddSettingSerializer
+from .serializers import (ContactSerilizer, Privacypolicyserializer, TOSserializer, TopicPollSerializer,UserPollSerializer,FeedbackSerializer,
+SettingsSerializer,ContactusSerializer,AddSettingSerializer,Aboutusserializer)
 from .models import Feedback,TopicPoll,Settings,ContactUs,AddSetting
 from rest_framework import mixins
 from rest_framework import generics, status, views, permissions
 from rest_framework import pagination
 from django.shortcuts import get_object_or_404
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+from rest_framework.decorators import api_view
+
+
+
+# from django.clinictopic.poll import serializers
 
 
 class TwentyPagination(pagination.PageNumberPagination):       
@@ -89,17 +98,95 @@ class Feedbackview(generics.ListCreateAPIView):
 
 
 class SettingsViewSet(generics.ListCreateAPIView):
-    queryset = Settings.objects.all().order_by('id')
-    serializer_class = SettingsSerializer
-    pagination_class = None
-    permission_classes = (IsAuthenticated,)
+#@renderer_classes(JSONRenderer)    
+#@csrf_exempt
+    # @api_view(['GET', 'POST', ])
+    # def get_serializer(self, *args, **kwargs):
+    #     print("hihihihi")
+    #     """ if an array is passed, set serializer to many """
+    #     if isinstance(kwargs.get('data', {}), list):
+    #         kwargs['many'] = True
+    #     if self.request.method == 'GET':
+    #         queryset = Settings.objects.all().order_by('id')
+    #         serializer_class = SettingsSerializer(queryset,many='True')
+    #         # pagination_class = None
+    #         # permission_classes = (IsAuthenticated,)
+    #         return Response(serializer_class.data)
+    #     if self.request.method =='POST':
+    #         data = JSONParser().parse(self.request)
+    #         print(data)
+    #         serializer = SettingsSerializer(data=data)
+    #         if serializer.is_valid():
+    #             serializer.save()
+    #             return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+    #         return JsonResponse(serializer.errors, status=400)
+    # def get_queryset(self):
+    #     return Settings.objects.all().order_by('-id')
+    serializer_class=SettingsSerializer
+    pagination_class=None
+    @csrf_exempt
+    def perform_create(self,serializer):
+        serializer.save()
+    @csrf_exempt
+    def get_queryset(self):
+        return Settings.objects.filter()
+
 
 class ContactusView(viewsets.ModelViewSet):
     queryset = ContactUs.objects.all().order_by('id')
     serializer_class = ContactusSerializer
     permission_classes = (IsAuthenticated,)
+    
 
 class AddSettingView(viewsets.ModelViewSet):
     queryset = AddSetting.objects.all().order_by('id')
     serializer_class = AddSettingSerializer
     permission_classes = (IsAuthenticated,)
+
+class AboutusApiview(generics.ListCreateAPIView):
+    serializer_class = Aboutusserializer
+    pagination_class = None
+    # permission_classes = (IsAuthenticated,)
+    @csrf_exempt
+    def perform_create(self, serializer): 
+
+        # us = .objects.filter(user_id =self.request.user).delete()
+        serializer.save()
+    @csrf_exempt
+    def get_queryset(self):
+        return Settings.objects.filter() 
+
+
+class ContactusApiView(generics.ListCreateAPIView):
+    serializer_class =ContactSerilizer
+    pagination_class=None
+    @csrf_exempt
+    def perform_create(self,serializer):
+         serializer.save()
+    @csrf_exempt
+    def get_queryset(self):
+        return Settings.objects.filter()
+
+class PrivacyPolicyApiView(generics.ListCreateAPIView):
+    serializer_class=Privacypolicyserializer
+    pagination_class=None
+    @csrf_exempt
+    def perform_create(self,serializer):
+        serializer.save()
+    @csrf_exempt
+    def get_queryset(self):
+        return Settings.objects.filter()
+
+class TOSapiView(generics.ListCreateAPIView):
+    serializer_class=TOSserializer
+    pagination_class=None
+    @csrf_exempt
+    def perform_create(self,serializer):
+        #   tos=request.tos
+        #if not Settings.objects.filter().exists():
+        serializer.save()
+        print("Doneeeeeeeee")
+        #serializer.update()
+    @csrf_exempt
+    def get_queryset(self):
+        return Settings.objects.filter()
