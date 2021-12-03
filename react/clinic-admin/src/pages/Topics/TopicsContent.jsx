a0a60b2b96c478cec99df92920eda8a24781bdca
sajitha <sajitha@metrictreelabs.com>
2021-12-02 22:02:24 -0800

    bug fixed

--- react/clinic-admin/src/pages/Topics/TopicsContent.jsx
+++ react/clinic-admin/src/pages/Topics/TopicsContent.jsx
@@ -1,39 +1,39 @@
 import React, { useEffect, useState } from "react";
-import { Button, Card, Table, Space, Drawer, Popconfirm, message , Spin, Form } from "antd";
+import { Button, Card, Table, Space, Drawer, Popconfirm, message, Spin, Form } from "antd";
 import "antd/dist/antd.css";
 import ModalContent from "./ModalContent";
 import { useDispatch, useSelector } from "react-redux";
 import { Icon, IconButton } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
-import { getTopic , deleteTopic, postTopic, updateTopic  } from "../../actions/topic";
+import { getTopic, deleteTopic, postTopic, updateTopic } from "../../actions/topic";
 import { logout } from "../../actions/auth.js"
 
 const TopicsContent = (props) => {
   const [showDrawer, setShowDrawer] = useState(false);
   const [drawerType, setDrawerType] = useState("");
   const [expended, setExpended] = useState()
-  const [data , setData] = useState({});
-  const { topicList, success, error, page} = useSelector(state => state.topic);
+  const [data, setData] = useState({});
+  const { topicList, success, error, page } = useSelector(state => state.topic);
   const [current, setCurrent] = useState(1);
-  const [pageSize , setPageSize] = useState(4);
+  const [pageSize, setPageSize] = useState(4);
   const [slNo, setSlNo] = useState(0);
   const dispatch = useDispatch();
   const accessToken = localStorage.getItem("accessToken");
   let history = useHistory();
-  
+
   useEffect(() => {
     dispatch(getTopic(page))
   }, [])
 
   useEffect(() => {
-    if(success) {
+    if (success) {
       message.success(success);
       dispatch(getTopic(page))
-      dispatch({type: 'RESET_DATA'})
+      dispatch({ type: 'RESET_DATA' })
     }
-    else if(error) {
+    else if (error) {
       message.error(error);
-      dispatch({type: 'RESET_DATA'})
+      dispatch({ type: 'RESET_DATA' })
       const clearToken = localStorage.clear();
       dispatch(logout());
       history.push("/login");
@@ -41,11 +41,15 @@ const TopicsContent = (props) => {
   }, [success, error])
 
   useEffect(() => {
-    if (accessToken === null  || accessToken === undefined ){
+    if (accessToken === null || accessToken === undefined) {
       history.push("/login");
     }
   }, [])
 
+  const sessionlogout = () => {
+    const clearToken = localStorage.clear();
+    dispatch(logout());
+  }
 
   const onClose = () => {
     setShowDrawer(false);
@@ -66,42 +70,42 @@ const TopicsContent = (props) => {
     message.error('Cancelled');
   }
 
-  const onFormSubmit = (newData, form_data, form_data_back , form_data2, form_data3, image_data) => {
+  const onFormSubmit = (newData, form_data, form_data_back, form_data2, form_data3, image_data) => {
     onClose();
-    if(drawerType == 'edit') {
-      dispatch(updateTopic(data.id, newData, form_data, form_data_back ,form_data2,form_data3, image_data));
+    if (drawerType == 'edit') {
+      dispatch(updateTopic(data.id, newData, form_data, form_data_back, form_data2, form_data3, image_data));
     } else {
-      dispatch(postTopic(newData, form_data, form_data_back , form_data2 ,form_data3 , image_data));
+      dispatch(postTopic(newData, form_data, form_data_back, form_data2, form_data3, image_data));
     }
   }
 
   const topicGenerator = () => {
     let serialNo = pageSize * slNo;
     const items = [];
-    topicList && topicList.results && topicList.results.map((item , key) => {
+    topicList && topicList.results && topicList.results.map((item, key) => {
       serialNo++;
       const topics = [];
       const specData = [];
       const subspec = [];
       const topicSubspec = [];
       const images = [];
-        item.topic_topic && item.topic_topic.map(item => {
-          topics.push({spec_id: item.spec_id.id});
-          specData.push({value: item.spec_id.id , label: item.spec_id.name})
-        });
+      item.topic_topic && item.topic_topic.map(item => {
+        topics.push({ spec_id: item.spec_id.id });
+        specData.push({ value: item.spec_id.id, label: item.spec_id.name })
+      });
 
-        item.topic_image && item.topic_image.map(item => {
-          images.push({id: item.id, image: item.image});
-        })
-       
-        item.topic_subspec  && item.topic_subspec.map(item =>{
-          subspec.push({  value: `${item.subspec_id.name}_${item.subspec_id.id}`});
-          topicSubspec.push({ subspec_id: item.subspec_id.id})
-        })
-    
+      item.topic_image && item.topic_image.map(item => {
+        images.push({ id: item.id, image: item.image });
+      })
+
+      item.topic_subspec && item.topic_subspec.map(item => {
+        subspec.push({ value: `${item.subspec_id.name}_${item.subspec_id.id}` });
+        topicSubspec.push({ subspec_id: item.subspec_id.id })
+      })
+
       items.push({
         sl_no: serialNo,
-        key:item.id,
+        key: item.id,
         id: item.id,
         title: item.title,
         title1: item.format === '1' ? item.title : null,
@@ -111,7 +115,7 @@ const TopicsContent = (props) => {
         title3: item.format === '3' ? item.title : null,
         description3: item.format == '3' ? item.description : null,
         category_title: item.category_id.title,
-        category_data: {value: item.category_id.id, label: item.category_id.title},
+        category_data: { value: item.category_id.id, label: item.category_id.title },
         category_id: item.category_id.id,
         description: item.description,
         source_url: item.source_url,
@@ -119,44 +123,44 @@ const TopicsContent = (props) => {
         topic_topic: topics,
         publishingtime: item.publishingtime,
         publishtype: item.publishingtime > new Date() ? "later" : "now",
-        deliverytype:item.deliverytype,
+        deliverytype: item.deliverytype,
         topic_val: subspec,
         topic_subspec: topicSubspec,
         P: item.deliverytype,
         media_type: item.media_type !== null ? 'image' : item.video_type !== null ? 'video' : '',
         old_image: images,
-        video_url:item.video_url,
+        video_url: item.video_url,
         pdfFront: item.pdf,
         pdfBack: item.pdfsecond,
         pdfSecond: item.pdf,
-        pdfThird:item.pdf,
-        pdfUrlSecond:item.pdf,
-        pdfUrlThird:item.pdf,
+        pdfThird: item.pdf,
+        pdfUrlSecond: item.pdf,
+        pdfUrlThird: item.pdf,
         format: item.format,
-        external_url:item.external_url,
+        external_url: item.external_url,
         external_url2: item.external_url,
         external_url3: item.external_url,
         email: item.author && item.author.email,
         username: item.author && item.author.username,
         published_status: item.published,
         imageFormData: []
-      }) 
-      
+      })
+
     });
     return items;
   }
 
   const handleChange = (page, size, sorter) => {
     setCurrent(page)
-    setSlNo(page-1)
+    setSlNo(page - 1)
     dispatch(getTopic(page));
   }
 
-  const pagination =  {
-    current ,
+  const pagination = {
+    current,
     pageSize,
     showSizeChanger: false,
-    onChange: (page, pageSize, sorter) => {handleChange(page, pageSize, sorter)},
+    onChange: (page, pageSize, sorter) => { handleChange(page, pageSize, sorter) },
     total: topicList.count
   }
 
@@ -182,7 +186,7 @@ const TopicsContent = (props) => {
       align: "center",
       render: (text, record) => (
         <Space size="middle">
-          <Button type="link" onClick={() => {onEdit(record)}}>
+          <Button type="link" onClick={() => { onEdit(record) }}>
             Edit
           </Button>
           <Popconfirm
@@ -201,27 +205,31 @@ const TopicsContent = (props) => {
 
   return (
     <div style={{ margin: "10px" }}>
-      <Card
-        title="Topics"
-        extra={
-          <IconButton onClick={onAdd}>
-            <Icon>add</Icon>
-          </IconButton>
-        } style={{ width: "100%" }} >
-        {topicList && topicList.results  &&  page == current ?
-        (<Table expandedRowKeys={[expended]} columns={columns} pagination={pagination} dataSource={topicGenerator()} />) : (<div className="spinner"><Spin tip="Loading..." style = {{align:"center"}}/></div>) }
-      </Card>
-      <Drawer
-        title={drawerType === "edit" ? "Edit Topic" : drawerType === "add" ? "Add Topic" : "" }
-        placement="right"
-        width={750}
-        closable={true}
-        onClose={onClose}
-        visible={showDrawer}
-        key="drawer"
-      >
-        <ModalContent drawerType={drawerType} editData={(drawerType === 'edit') ? data : null} onFormSubmit={onFormSubmit}/>
-      </Drawer>
+      {accessToken === null || accessToken === undefined ? (sessionlogout()) :
+        (<>
+          <Card
+            title="Topics"
+            extra={
+              <IconButton onClick={onAdd}>
+                <Icon>add</Icon>
+              </IconButton>
+            } style={{ width: "100%" }} >
+            {topicList && topicList.results && page == current ?
+              (<Table expandedRowKeys={[expended]} columns={columns} pagination={pagination} dataSource={topicGenerator()} />) : (<div className="spinner"><Spin tip="Loading..." style={{ align: "center" }} /></div>)}
+          </Card>
+          <Drawer
+            title={drawerType === "edit" ? "Edit Topic" : drawerType === "add" ? "Add Topic" : ""}
+            placement="right"
+            width={750}
+            closable={true}
+            onClose={onClose}
+            visible={showDrawer}
+            key="drawer"
+          >
+            <ModalContent drawerType={drawerType} editData={(drawerType === 'edit') ? data : null} onFormSubmit={onFormSubmit} />
+          </Drawer>
+        </>
+        )}
     </div>
   );
 };
--- react/clinic-admin/src/reducers/auth.js
+++ react/clinic-admin/src/reducers/auth.js
@@ -20,7 +20,6 @@ const initialState = {
 
 
 export default function (state = initialState, action) {
-  console.log("error", action);
   const { type, payload } = action;
   switch (type) {
     case LOGIN_SUCCESS:
