c63252f3f075e1e93dce20ac98c54a8c86e175bb
ajith <ajith@metrictreelabs.com>
2022-03-31 21:38:04 +0530

    changes

--- django/clinictopic/topics/serializers.py
+++ django/clinictopic/topics/serializers.py
@@ -383,9 +383,17 @@ class authorserializer(serializers.ModelSerializer):
             return topic
 
 
+class FilteredListSerializer(serializers.ListSerializer):
+
+    def to_representation(self, data):
+        data = data.filter(title__icontains=self.context['key'])
+        return super(FilteredListSerializer, self).to_representation(data)
+
+
 class TopicSearchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Topics
+        list_serializer_class = FilteredListSerializer
         fields = ['id','title']
     # def to_representation(self, instance):
     #     response = super().to_representation(instance)
@@ -393,13 +401,12 @@ class TopicSearchSerializer(serializers.ModelSerializer):
     #     return response
 #search topic suggestion
 class categoryTopicSerializer(serializers.ModelSerializer):
-    topic_category = serializers.SerializerMethodField()
-
-    def get_topic_category(self, product):
-        key = self.context.get("key")
-        qs = Topics.objects.filter(title__icontains=pk)
-        serializer = TopicSearchSerializer(instance=qs, many=True)
-        return serializer.data
+    topic_category =TopicSearchSerializer(many=True)
+    # def get_topic_category(self, product):
+    #     key = self.context.get("key")
+    #     qs = Topics.objects.filter(title__icontains=pk)
+    #     serializer = TopicSearchSerializer(instance=qs, many=True)
+    #     return serializer.data
     class Meta:
         model = Categoeries
         fields = ['id','title','topic_category']
